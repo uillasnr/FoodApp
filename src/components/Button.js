0483ce63ef229f7e17da08e1935a9a76ace9596a
uillasnr <wiliasreis@hotmail.com>
2024-10-26 21:08:33 -0300

    fix: charging state and opacity

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,23 @@
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import Color from "../styles/Color";
 
 export default Button = (props) => {
   return (
-    <TouchableOpacity style={styles.btn} onPress={props.onPress}>
-      <Text style={styles.label}>{props.label}</Text>
+    <TouchableOpacity
+      style={[styles.btn, props.isLoading ? styles.loading : ""]}
+      disabled={props.isLoading}
+      onPress={props.onPress}
+    >
+      {props.isLoading ? (
+        <ActivityIndicator color={Color.COLORS.white} />
+      ) : (
+        <Text style={styles.label}>{props.label}</Text>
+      )}
     </TouchableOpacity>
   );
 };
@@ -14,11 +27,14 @@ const styles = StyleSheet.create({
     width: "100%",
     backgroundColor: Color.COLORS.red,
     borderRadius: 6,
+    padding: 14,
   },
   label: {
     textAlign: "center",
-    padding: 14,
     color: Color.COLORS.white,
     fontSize: Color.FONT_SIZE.md,
+  },
+  loading: {
+    opacity: 0.5,
   },
 });
